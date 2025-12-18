3965a8ae3c9208989a9ee50fa3f52ff07f755667
Abdullah Tanzeel <abdullahtanzeel1224@gmail.com>
2025-12-18 14:01:57 +0000

    Added Incremental materialization to bronze orders

--- models/bronze/bronze_orders.sql
+++ models/bronze/bronze_orders.sql
@@ -1,3 +1,16 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'id'
+    )
+}}
+
 SELECT
 *
 FROM {{ source('landing', 'orders') }}
+
+{% if is_incremental() -%}
+
+WHERE created_at > (SELECT COALESCE(MAX(created_at), '1900-01-01') FROM {{ this }})
+
+{% endif %}
